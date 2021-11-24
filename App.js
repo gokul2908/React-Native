eaef2a39f6cd9ac9ed7c26f8b2801f1efc2fbb21
Gokulkumar M <gokulkumar2908@gmail.com>
2021-11-24 21:12:09 +0530

    component added

--- App.js
+++ App.js
@@ -1,41 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native djsklfalsdjfls dfsjkldfsd fkljsd flkjsdflkjsdflj sdfj
- *
- * @format
- * @flow strict-local
- */
-
 import React, { useState } from "react";
-import type { Node } from "react";
-import {
-  Button,
-  SafeAreaView,
-  ScrollView,
-  Pressable,
-  Alert,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen";
-
-function Component1({ value, onclick }) {
+let operator_arr = ["*", "/", "+", "-"];
+// button component
+function Component_button({ value, onclick, display_val }) {
   return (
-    <View>
-      <Button onPress={() => onclick(input_val + "3")} title={value} />
-    </View>
+    <Pressable
+      onPress={() =>
+        operator_arr.includes(value)
+          ? addOperator(display_val, onclick, value)
+          : onclick(display_val + String(value))
+      }
+      style={styles.Operatorbutton}
+    >
+      <Text style={styles.textWeightage}>{value}</Text>
+    </Pressable>
   );
 }
+
 const App = () => {
   const [input_val, set_val] = useState("");
 
@@ -43,109 +25,55 @@ const App = () => {
     <View style={styles.flexcol}>
       <Pressable onPress={() => set_val("")} style={styles.display}>
         <Text style={styles.textWeightage}>{input_val}</Text>
-        {/* <Component1 value={input_val} onclick={set_val}/> */}
       </Pressable>
-
       <View style={styles.flexrow}>
-        <Pressable
-          onPress={() => set_val(input_val + "7")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>7</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => set_val(input_val + "8")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>8</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => set_val(input_val + "9")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>9</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => addOperator(input_val, set_val, "/")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>/</Text>
-        </Pressable>
+        {[7, 8, 9, "/"].map((each) => (
+          <Component_button
+            key={each}
+            display_val={input_val}
+            onclick={set_val}
+            value={each}
+          />
+        ))}
+      </View>
+      <View style={styles.flexrow}>
+        {[4, 5, 6, "*"].map((each) => (
+          <Component_button
+            key={each}
+            display_val={input_val}
+            onclick={set_val}
+            value={each}
+          />
+        ))}
+      </View>
+      <View style={styles.flexrow}>
+        {[1, 2, 3, "-"].map((each) => (
+          <Component_button
+            key={each}
+            display_val={input_val}
+            onclick={set_val}
+            value={each}
+          />
+        ))}
       </View>
       <View style={styles.flexrow}>
         <Pressable
-          onPress={() => set_val(input_val + "4")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>4</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => set_val(input_val + "5")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>5</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => set_val(input_val + "6")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>6</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => addOperator(input_val, set_val, "*")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>x</Text>
-        </Pressable>
-      </View>
-      <View style={styles.flexrow}>
-        <Pressable
-          onPress={() => set_val(input_val + "1")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>1</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => set_val(input_val + "2")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>2</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => set_val(input_val + "3")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>3</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => addOperator(input_val, set_val, "-")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>-</Text>
-        </Pressable>
-      </View>
-      <View style={styles.flexrow}>
-        <Pressable
-          onPress={() => set_val(input_val + ".")}
-          style={styles.button}
+          onPress={() => add_dot(input_val, set_val)}
+          style={styles.equal2button}
         >
           <Text style={styles.textWeightage}>.</Text>
         </Pressable>
-        <Pressable
-          onPress={() => set_val(input_val + "0")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>0</Text>
-        </Pressable>
-        <Pressable
-          onPress={() => addOperator(input_val, set_val, "+")}
-          style={styles.button}
-        >
-          <Text style={styles.textWeightage}>+</Text>
-        </Pressable>
+        {[0, "+"].map((each) => (
+          <Component_button
+            key={each}
+            display_val={input_val}
+            onclick={set_val}
+            value={each}
+          />
+        ))}
         <Pressable
           onPress={() => resultGenerator(input_val, set_val)}
-          style={styles.button}
+          style={styles.equal2button}
         >
           <Text style={styles.textWeightage}>=</Text>
         </Pressable>
@@ -153,8 +81,21 @@ const App = () => {
     </View>
   );
 };
+function add_dot(input_data, set_data) {
+  index = input_data.length - 1;
+  while (index) {
+    if (operator_arr.includes(input_data[index])) {
+      set_data(input_data + ".");
+    } else if (input_data[index] == ".") {
+      return;
+    }
+    index -= 1;
+  }
+  set_data(input_data + ".");
+}
 
 function resultGenerator(input_data, set_data) {
+  if (!check_for_error(input_data)) return;
   try {
     set_data(String(eval(input_data)));
   } catch (err) {
@@ -162,8 +103,26 @@ function resultGenerator(input_data, set_data) {
   }
 }
 
+function check_for_error(val) {
+  if (val.length == 0 || operator_arr.includes(val[val.length - 1])) {
+    return false;
+  }
+  let index = 0;
+  while (index < val.length) {
+    if (val[index] == ".") {
+      index += 1;
+      if (index < val.length && val[index] == ".") {
+        return false;
+      }
+    }
+    index += 1;
+  }
+  return true;
+}
+
 function addOperator(input_data, set_data, val) {
-  if (["*", "/", "+", "-"].includes(input_data[input_data.length - 1])) {
+  if (input_data.length == 0) return;
+  if (operator_arr.includes(input_data[input_data.length - 1])) {
     if (input_data[input_data.length - 1] != val) {
       set_data(input_data.replace(/.$/, val));
     }
@@ -175,22 +134,22 @@ function addOperator(input_data, set_data, val) {
 const styles = StyleSheet.create({
   textWeightage: {
     fontWeight: "bold",
-    fontSize: 60,
+    fontSize: 40,
     color: "white",
     textAlignVertical: "center",
     alignSelf: "center",
   },
   display: {
-    fontSize: 60,
-    height: "18%",
+    fontSize: 30,
+    height: "50%",
     backgroundColor: "black",
   },
   button: {
     backgroundColor: "#2e2e2e",
     paddingHorizontal: 8,
-    paddingVertical: 6,
+    paddingVertical: 9,
     borderRadius: 22,
-    height: "80%",
+    height: "90%",
     width: "22%",
     textAlignVertical: "center",
     margin: 5,
@@ -198,15 +157,33 @@ const styles = StyleSheet.create({
   flexrow: {
     flexDirection: "row",
     flexWrap: "wrap",
-    height: "20%",
+    height: "12%",
   },
   flexcol: {
     flexDirection: "column",
     height: "100%",
     marginTop: 4,
+    backgroundColor: "black",
   },
-  buttontext: {
+  Operatorbutton: {
+    backgroundColor: "#d3de59",
+    paddingHorizontal: 8,
+    paddingVertical: 9,
+    borderRadius: 22,
+    height: "90%",
+    width: "22%",
     textAlignVertical: "center",
+    margin: 5,
+  },
+  equal2button: {
+    backgroundColor: "#e63e2c",
+    paddingHorizontal: 8,
+    paddingVertical: 9,
+    borderRadius: 22,
+    height: "90%",
+    width: "22%",
+    textAlignVertical: "center",
+    margin: 5,
   },
 });
 
